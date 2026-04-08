07c7bb878c93bfde00e8b9f07be82667fd217c5d
reema-santos <reema.santos21@gmail.com>
2026-04-08 19:11:25 +1000

    Game header

--- Board.cpp
+++ Board.cpp
@@ -54,3 +54,38 @@ void Board::DeletePossibleLines() {
 bool Board::IsFreeBlock(int pX, int pY) {
     return mBoard[pX][pY] == POS_FREE;
 }
+
+// Specify position in pixels to draw blocks to screen
+int Board::GetXPosInPixels(int pPos) {
+    // Centre of board (pixels) - Half of board (blocks to pixels)
+    // Offset by adding the x position (blocks to pixels)
+    // Multiplying by BLOCK_SIZE converts to pixels
+    return ((BOARD_POSITION - (BLOCK_SIZE * (BOARD_WIDTH/2))) + (pPos * BLOCK_SIZE));
+}
+
+int Board::GetYPosInPixels(int pPos) {
+    // Height of screen (pixels) - Height of board (blocks to pixels)
+    // Offset by adding the y position (blocks to pixels)
+    return ((mScreenHeight - (BLOCK_SIZE * BOARD_HEIGHT)) + (pPos * BLOCK_SIZE));
+}
+
+bool Board::IsPossibleMovement(int pX, int pY, int pPiece, int pRotation) {
+    // Iterate through piece matrix 
+    for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; ++i1, ++i2) {
+        for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; ++j1, ++j2) {
+            if (i1 < 0 || i1 > BOARD_WIDTH - 1 || j2 > BOARD_HEIGHT) {
+                if (mPieces->GetBlockType(pPiece, pRotation, j2, i2) != 0) {
+                    return false;
+                }
+            } 
+
+            if (j1 >= 0) {
+                if ((mPieces->GetBlockType(pPiece, pRotation, j2, i2) != 0) && (!IsFreeBlock(i1, j1))) {
+                    return false;
+                }
+            }
+        }
+    }
+    
+    return true;
+}
---
+++ Game.h
@@ -0,0 +1,32 @@
+#ifndef _GAME_
+#define _GAME_
+
+#include "Board.h"
+#include "Pieces.h"
+#include "IO.h"
+#include <time.h>
+
+#define WAIT_TIME 700 // Number of milliseconds that the piece remains before going 1 block down
+
+class Game {
+    public:
+        Game(Board* pBoard, Pieces* pPieces, IO* pIO, int pScreenHeight);
+        void DrawScene();
+        void CreateNewPiece();
+        int mPosX, mPosY;
+        int mPiece, mRotation;
+
+    private:
+        int mScreenHeight;
+        int mNextPosX, mNextPosY;
+        int mNextPiece, mNextRotation;
+        Board* mBoard;
+        Pieces* mPieces;
+        IO* mIO;
+        int GetRand(int pA, int pB);
+        void InitGame();
+        void DrawPiece(int pX, int pY, int pPiece, int pRotation);
+        void DrawBoard();
+};
+
+#endif
