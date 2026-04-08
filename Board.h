205fbc7609bb2729be27202d8801f9f440398cb2
reema-santos <reema.santos21@gmail.com>
2026-04-08 18:41:26 +1000

    Getters for pixel positions and movement possible checker

--- Board.h
+++ Board.h
@@ -3,14 +3,14 @@
 
 #include "Pieces.h"
 
-#define BOARD_LINE_WIDTH 6
-#define BLOCK_SIZE 16
-#define BOARD_POSITION 320
-#define BOARD_WIDTH 10
-#define BOARD_HEIGHT 20
-#define MIN_VERTICAL_MARGIN 20
-#define MIN_HORIZONTAL_MARGIN 20
-#define PIECE_BLOCKS 5
+#define BOARD_LINE_WIDTH 6 // Width of delimiters to board
+#define BLOCK_SIZE 16 // Width and height of a block
+#define BOARD_POSITION 320 // Centre position of board from the left of the screen
+#define BOARD_WIDTH 10 // Board width (blocks)
+#define BOARD_HEIGHT 20 // Board height (blocks)
+#define MIN_VERTICAL_MARGIN 20 // Minimum vertical board limit
+#define MIN_HORIZONTAL_MARGIN 20 // Minimum horizontal board limit
+#define PIECE_BLOCKS 5 // Number of horizonal and vertical blocks of a matrix piece
 
 class Board {
     public:
