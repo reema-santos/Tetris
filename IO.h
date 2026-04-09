36ed2ae0830c2cf9262c7e9b27c4c14d03eafbb1
reema-santos <reema.santos21@gmail.com>
2026-04-09 12:12:54 +1000

    IO class to draw sceen

--- IO.h
+++ IO.h
@@ -1,25 +1,19 @@
 #ifndef _IO_
 #define _IO_
 
-// —— Includes —–
-
-// #ifndef LINUX
-// #include "SDL/include/SDL.h"
-// #include "SDL/SDL_GfxPrimitives/SDL_gfxPrimitives.h"
-// #else
-// #include <SDL/SDL.h>
-// #include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"
-// #endif
-// #pragma comment (lib, "SDL/lib/SDL.lib")
-// #pragma comment (lib, "SDL/SDL_GfxPrimitives/SDL_GfxPrimitives_Static.lib")
+#ifndef LINUX
+#include "SDL/include/SDL.h"
+#include "SDL/SDL_GfxPrimitives/SDL_gfxPrimitives.h"
+#else
+#include <SDL/SDL.h>
+#include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"
+#endif
+#pragma comment (lib, "SDL/lib/SDL.lib")
+#pragma comment (lib, "SDL/SDL_GfxPrimitives/SDL_GfxPrimitives_Static.lib")
 
 // —— Enums —–
 
 enum color {BLACK, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE, COLOR_MAX}; // Colors
-
-// ——————————————————————————–
-// IO
-// ——————————————————————————–
 
 class IO
 {
